aeffdd36299899dabe322ade61733d72a23cb2aa
Samuel wanza <s.wanza@alustudent.com>
2023-07-01 11:15:54 +0200

    implemented API functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './styles.css';
+import createScore from './modules/createScore';
+import loadContent from './modules/loadContent';
+
+loadContent();
+
+document.getElementById('refresh-button').addEventListener('click', () => {
+  window.location.reload();
+});
+document.getElementById('submit-button').addEventListener('click', () => {
+  const user = document.getElementById('user');
+  const userscore = document.getElementById('userscore');
+  if (user.value !== '' && userscore.value !== '') {
+    createScore(user.value, userscore.value);
+  }
+});
---
+++ src/modules/gameData.js
@@ -0,0 +1,3 @@
+const gameID = '9uv5HhGpDRDFzk8NXN4u';
+const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/:${gameID}/scores/`;
+export default url;
